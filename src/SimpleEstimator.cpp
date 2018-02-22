631642a2bf81c30e56e16bc4e2642c5b4a52878d
George Park <george--park@hotmail.com>
2018-02-22 15:37:31 +0100

    Correctly estimates noPaths on single edge query
    Correctly estimates noPaths on single edge query, only changed prepare();

--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -5,6 +5,8 @@
 #include "SimpleGraph.h"
 #include "SimpleEstimator.h"
 
+std::map<uint32_t , cardStat> first;
+
 SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g){
 
     // works only with SimpleGraph
@@ -15,11 +17,26 @@ void SimpleEstimator::prepare() {
 
     // do your prep here
 
+    for(int noLabels = 0; noLabels < graph -> getNoLabels(); noLabels++) {
+        cardStat test {0,0,0};
+        first[noLabels] = test;
+    }
+
+    for(int source = 0; source < graph->getNoVertices(); source++) {
+        for (auto labelTarget : graph->adj[source]) {
+
+            auto label = labelTarget.first;
+
+            first[label].noPaths ++;
+
+        }
+    }
+
 }
 
 cardStat SimpleEstimator::estimate(RPQTree *q) {
 
     // perform your estimation here
 
-    return cardStat {0, 0, 0};
+    return first[0];
 }
