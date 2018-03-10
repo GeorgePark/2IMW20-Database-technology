51e2b1f674f28257cb6e9627810a1a3e9a821bd9
George Park <george--park@hotmail.com>
2018-03-10 13:43:52 +0100

    Using inverse actually makes the result worse
    Lol

--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -24,44 +24,10 @@ void SimpleEstimator::prepare() {
 
     // do your prep here
 
-    //int edgesPerLabel [graph->getNoLabels()];
-    //int inDegreePerLabel [graph->getNoLabels()];
-    //int outDegreePerLabel [graph->getNoLabels()];
-
     std::list<uint32_t> uniqueNodesForLabel;
 
     for(int noLabels = 0; noLabels < graph->getNoLabels(); noLabels++) {
         est_result[noLabels] = cardStat {0, 0, 0};
-
-        //edgesPerLabel[noLabels] = 0;
-        //inDegreePerLabel[noLabels] = 0;
-        //outDegreePerLabel[noLabels] = 0;
-
-        /*for (int source = 0; source < graph->getNoVertices(); source++) {
-            for (auto labelSource : graph->adj[source]) {
-                bool found = (std::find(uniqueNodesForLabel.begin(), uniqueNodesForLabel.end(), labelSource.second) !=
-                              uniqueNodesForLabel.end());
-                if (labelSource.first == noLabels && !found) {
-                    est_result[noLabels].noIn++;
-                    //outDegreePerLabel[noLabels]++;
-                    uniqueNodesForLabel.push_back(labelSource.second);
-                }
-            }
-        }
-        uniqueNodesForLabel.clear();
-        for (int target = 0; target < graph->getNoVertices(); target++) {
-            for (auto labelTarget : graph->reverse_adj[target]) {
-                bool found = (std::find(uniqueNodesForLabel.begin(), uniqueNodesForLabel.end(), labelTarget.second) !=
-                              uniqueNodesForLabel.end());
-                if (labelTarget.first == noLabels && !found) {
-                    est_result[noLabels].noOut++;
-                    //inDegreePerLabel[noLabels]++;
-                    uniqueNodesForLabel.push_back(labelTarget.second);
-                }
-            }
-        }
-        uniqueNodesForLabel.clear();*/
-
     }
 
     for (int source = 0; source < graph->getNoVertices(); source++) {
@@ -83,12 +49,6 @@ void SimpleEstimator::prepare() {
     for (int noLabels = 0; noLabels < graph->getNoLabels(); noLabels++) {
         est_result[noLabels].noOut = hasLabel[noLabels].size();
     }
-    /*
-    for (int noLabels = 0; noLabels < graph->getNoLabels(); noLabels++) {
-        uint32_t helper = (uint32_t)(((float)(est_result[noLabels].noPaths) / (float)(graph->getNoEdges())) * graph->getNoVertices());
-        est_result[noLabels].noOut = helper;
-        est_result[noLabels].noIn = helper;
-    }*/
 
 }
 
@@ -101,15 +61,12 @@ cardStat SimpleEstimator::estimate(RPQTree *q) {
     std::smatch matches;
 
     uint32_t label;
-    bool inverse = false;
 
     if (q->isLeaf()) {
         if (std::regex_search(q->data, matches, directLabel)) {
             label = (uint32_t) std::stoul(matches[1]);
-            inverse = false;
         } else if (std::regex_search(q->data, matches, inverseLabel)) {
             label = (uint32_t) std::stoul(matches[1]);
-            inverse = true;
         } else {
             std::cerr << "Label parsing failed!" << std::endl;
             return {0, 0, 0};
@@ -124,12 +81,6 @@ cardStat SimpleEstimator::estimate(RPQTree *q) {
         leftGraph = SimpleEstimator::estimate(q->left);
         rightGraph = SimpleEstimator::estimate(q->right);
 
-
-        //float result = leftGraph.noPaths * (leftGraph.noIn * (float(rightGraph.noOut) / (float)(graph->getNoVertices())) * ((float)(rightGraph.noPaths) / (float)(rightGraph.noOut)));
-
-        //float result1 = (float)(leftGraph.noPaths * rightGraph.noPaths) / (float)(leftGraph.noOut);
-        //float result2 = (float)(leftGraph.noPaths * rightGraph.noPaths) / (float)(rightGraph.noOut);
-        //uint32_t final_result = std::min((int)(result1), (int)(result2));
         uint32_t result1 = (leftGraph.noPaths * rightGraph.noPaths) / leftGraph.noOut;
         uint32_t result2 = (leftGraph.noPaths * rightGraph.noPaths) / rightGraph.noOut;
 
@@ -137,11 +88,7 @@ cardStat SimpleEstimator::estimate(RPQTree *q) {
                          std::min(leftGraph.noIn, rightGraph.noIn)};
     }
 
-    if (inverse) {
-        cardStat inverseStats = {est_result[label].noIn, est_result[label].noPaths, est_result[label].noOut};
-        return inverseStats;
-    } else {
-        return est_result[label];
-    }
+
+    return est_result[label];
 
 }
