1ad4f2d872a8d3a4fde80017c189c2872be47296
GeorgePark <george--park@hotmail.com>
2018-03-30 14:30:11 +0200

    Should not add duplicate edges
    Should not add duplicate edges

--- src/SimpleGraph.cpp
+++ src/SimpleGraph.cpp
@@ -4,7 +4,7 @@
 
 #include "SimpleGraph.h"
 
-SimpleGraph::SimpleGraph(uint32_t n)   {
+SimpleGraph::SimpleGraph(uint32_t n) {
     setNoVertices(n);
 }
 
@@ -20,13 +20,13 @@ void SimpleGraph::setNoVertices(uint32_t n) {
 
 uint32_t SimpleGraph::getNoEdges() const {
     uint32_t sum = 0;
-    for (const auto & l : adj)
+    for (const auto &l : adj)
         sum += l.size();
     return sum;
 }
 
 // sort on the second item in the pair, then on the cardStats (ascending order)
-bool sortPairs(const std::pair<uint32_t,uint32_t> &a, const std::pair<uint32_t,uint32_t> &b) {
+bool sortPairs(const std::pair<uint32_t, uint32_t> &a, const std::pair<uint32_t, uint32_t> &b) {
     if (a.second < b.second) return true;
     if (a.second == b.second) return a.first < b.first;
     return false;
@@ -66,10 +66,10 @@ void SimpleGraph::setNoLabels(uint32_t noLabels) {
 }
 
 void SimpleGraph::addEdge(uint32_t from, uint32_t to, uint32_t edgeLabel) {
-    if(from >= V || to >= V || edgeLabel >= L)
+    if (from >= V || to >= V || edgeLabel >= L)
         throw std::runtime_error(std::string("Edge data out of bounds: ") +
-                                         "(" + std::to_string(from) + "," + std::to_string(to) + "," +
-                                         std::to_string(edgeLabel) + ")");
+                                 "(" + std::to_string(from) + "," + std::to_string(to) + "," +
+                                 std::to_string(edgeLabel) + ")");
     adj[from].emplace_back(std::make_pair(edgeLabel, to));
     reverse_adj[to].emplace_back(std::make_pair(edgeLabel, from));
 }
@@ -77,15 +77,15 @@ void SimpleGraph::addEdge(uint32_t from, uint32_t to, uint32_t edgeLabel) {
 void SimpleGraph::readFromContiguousFile(const std::string &fileName) {
 
     std::string line;
-    std::ifstream graphFile { fileName };
+    std::ifstream graphFile{fileName};
 
-    std::regex edgePat (R"((\d+)\s(\d+)\s(\d+)\s\.)"); // subject predicate object .
-    std::regex headerPat (R"((\d+),(\d+),(\d+))"); // noNodes,noEdges,noLabels
+    std::regex edgePat(R"((\d+)\s(\d+)\s(\d+)\s\.)"); // subject predicate object .
+    std::regex headerPat(R"((\d+),(\d+),(\d+))"); // noNodes,noEdges,noLabels
 
     // parse the header (1st line)
     std::getline(graphFile, line);
     std::smatch matches;
-    if(std::regex_search(line, matches, headerPat)) {
+    if (std::regex_search(line, matches, headerPat)) {
         uint32_t noNodes = (uint32_t) std::stoul(matches[1]);
         uint32_t noLabels = (uint32_t) std::stoul(matches[3]);
 
@@ -95,17 +95,20 @@ void SimpleGraph::readFromContiguousFile(const std::string &fileName) {
         throw std::runtime_error(std::string("Invalid graph header!"));
     }
 
-    //TODO: Do quicker loading using strtok?
-
     // parse edge data
-    while(std::getline(graphFile, line)) {
+    while (std::getline(graphFile, line)) {
 
-        if(std::regex_search(line, matches, edgePat)) {
+        if (std::regex_search(line, matches, edgePat)) {
             uint32_t subject = (uint32_t) std::stoul(matches[1]);
             uint32_t predicate = (uint32_t) std::stoul(matches[2]);
             uint32_t object = (uint32_t) std::stoul(matches[3]);
 
-            addEdge(subject, object, predicate);
+            auto item = std::make_pair(predicate, object);
+            if (std::find(adj[subject].begin(), adj[subject].end(), item) != adj[subject].end()) {
+                // Do nothing as item is already present
+            } else {
+                addEdge(subject, object, predicate);
+            }
         }
     }
     graphFile.close();
