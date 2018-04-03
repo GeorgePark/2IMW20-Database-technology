c9f0a981de547bb200291508c903cad776796815
GeorgePark <george--park@hotmail.com>
2018-04-03 23:28:42 +0200

    Revert "Tests"
    This reverts commit 03b630e233257536d22599b66bc996484d57a794.

--- src/SimpleEvaluator.cpp
+++ src/SimpleEvaluator.cpp
@@ -88,17 +88,13 @@ SimpleEvaluator::join(std::shared_ptr<SimpleGraph> &left, std::shared_ptr<Simple
         for (auto labelTarget : left->adj[leftSource]) {
 
             int leftTarget = labelTarget.second;
-            std::set<uint32_t > test;
             // try to join the left target with right source
             for (auto rightLabelTarget : right->adj[leftTarget]) {
 
                 auto rightTarget = rightLabelTarget.second;
-                if (test.count(rightTarget) == 0) {
-                    out->addEdge(leftSource, rightTarget, 0);
-                    test.insert(rightTarget);
-                }
+                out->addEdge(leftSource, rightTarget, 0);
+
             }
-            test.clear();
         }
     }
     return out;
@@ -161,8 +157,6 @@ cardStat SimpleEvaluator::evaluate(RPQTree *query) {
                 highest = current;
             }
         }
-        std::cout<<"\nChosen query: ";
-        newQuery->print();
         auto res = evaluate_aux(newQuery);
         cache[data] = SimpleEvaluator::computeStats(res);
         return cache[data];
